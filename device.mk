aa4286bbacf9812ed93244527e3c9d51ce762314
AdarshGrewal <adarshgrewal@gmail.com>
2022-02-19 16:35:05 +0530

    config: import stock WCNSS config
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- device.mk
+++ device.mk
@@ -212,6 +212,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
