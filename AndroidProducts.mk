4ebace62304713661aef16cc96904ff063c95c5f
Doctor Strange <faizanbashir380@gmail.com>
2022-02-21 11:50:29 +0000

    sweet: Bringup for AICP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_sweet.mk
+    $(LOCAL_DIR)/aicp_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_sweet-eng \
-    arrow_sweet-userdebug \
-    arrow_sweet-user
+    aicp_sweet-eng \
+    aicp_sweet-userdebug \
+    aicp_sweet-user
--- aicp_sweet.mk
+++ aicp_sweet.mk
@@ -5,23 +5,31 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common AICP configurations
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit ANX camera
 $(call inherit-product-if-exists, vendor/ANXCamera/config.mk)
 
-PRODUCT_NAME := arrow_sweet
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Ignore Neverallows on USER build
+SELINUX_IGNORE_NEVERALLOWS_ON_USER := true
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+
+PRODUCT_NAME := aicp_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-DEVICE_MAINTAINER := daniml3
-TARGET_INCLUDE_PIXEL_CHARGER := true
-TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
